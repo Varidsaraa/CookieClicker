6bd0727447af78f1db89c275b6e3df4581754a43
Varidsara Blackburn <91691298+Varidsaraa@users.noreply.github.com>
2022-02-03 12:20:54 -0500

    first commit

---
+++ cookie.py
@@ -0,0 +1,29 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("Cookie Clicker By Gogo")
+wn.bgcolor("white")
+
+wn.register_shape("cookie.gif")
+
+cookie = turtle.Turtle()
+cookie.shape("cookie.gif")
+cookie.speed(0)
+
+clicks = 0
+pen = turtle.Turtle()
+pen.hideturtle()
+pen.color("black")
+pen.penup()
+pen.goto(0, 200)
+pen.write(f"Clicks: {clicks}", align="center", font=("Arial", 32, "normal"))
+def clicked(x, y):
+    global clicks
+    clicks += 1
+    pen.clear()
+    pen.write(f"Clicks: {clicks}", align="center", font=("Arial", 32, "normal"))
+cookie.onclick(clicked)
+
+
+
+wn.mainloop()
